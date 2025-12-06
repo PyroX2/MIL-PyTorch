10b4ed0f890b39797dc04f0c258acd2b66218d5e
Jakub Wilk <kubawilk63@gmail.com>
2025-12-06 09:41:38 +0100

    Add parameters to metrics calculator classes

--- metrics.py
+++ metrics.py
@@ -7,16 +7,16 @@ from typing import List, Tuple
 
 
 class BinaryMetricsCalculator:
-    def __init__(self):
-        self.accuracy = BinaryAccuracy()
-        self.f1_score = BinaryF1Score()
+    def __init__(self, threshold: float = 0.5) -> None:
+        self.accuracy = BinaryAccuracy(threshold=threshold)
+        self.f1_score = BinaryF1Score(threshold=threshold)
         self.auprc = BinaryAUPRC()
         self.auroc = BinaryAUROC()
-        self.confusion_matrix = BinaryConfusionMatrix()
-        self.precision = BinaryPrecision()
-        self.recall = BinaryRecall()
+        self.confusion_matrix = BinaryConfusionMatrix(threshold=threshold)
+        self.precision = BinaryPrecision(threshold=threshold)
+        self.recall = BinaryRecall(threshold=threshold)
     
-    def calculate(self, outputs: List, targets: List) -> Tuple:
+    def calculate(self, outputs: List | torch.Tensor, targets: List | torch.Tensor) -> Tuple:
         if not isinstance(outputs, torch.Tensor):
             outputs = torch.tensor(outputs, dtype=torch.float32)
         else:
@@ -60,25 +60,30 @@ class BinaryMetricsCalculator:
 
 
 class MulticlassMetricsCalculator:
-    def __init__(self, num_classes):
-        self.accuracy = MulticlassAccuracy(num_classes=num_classes)
-        self.f1_score = MulticlassF1Score(num_classes=num_classes)
-        self.auprc = MulticlassAUPRC(num_classes=num_classes)
-        self.auroc = MulticlassAUROC(num_classes=num_classes)
+    def __init__(self, num_classes: int, avg_method: str = "micro") -> None:
+        assert avg_method in ["micro", "macro", "None", None], f'avg method should be in ["micro", "macro", "None", None], found {avg_method}'
+
+        if avg_method == "None":
+            avg_method = None
+
+        self.accuracy = MulticlassAccuracy(num_classes=num_classes, average=avg_method)
+        self.f1_score = MulticlassF1Score(num_classes=num_classes, average=avg_method)
+        self.auprc = MulticlassAUPRC(num_classes=num_classes, average=avg_method)
+        self.auroc = MulticlassAUROC(num_classes=num_classes, average=avg_method)
         self.confusion_matrix = MulticlassConfusionMatrix(num_classes=num_classes)
-        self.precision = MulticlassPrecision(num_classes=num_classes)
-        self.recall = MulticlassRecall(num_classes=num_classes)
+        self.precision = MulticlassPrecision(num_classes=num_classes, average=avg_method)
+        self.recall = MulticlassRecall(num_classes=num_classes, average=avg_method)
     
-    def calculate(self, outputs: List, targets: List) -> Tuple:
+    def calculate(self, outputs: List | torch.Tensor, targets: List | torch.Tensor) -> Tuple:
         if not isinstance(outputs, torch.Tensor):
-            outputs = torch.tensor(outputs, dtype=torch.float32)
+            outputs = torch.tensor(outputs, dtype=torch.long)
         else:
-            outputs = outputs.to(torch.float32)
+            outputs = outputs.to(torch.long)
             
         if not isinstance(targets, torch.Tensor):
-            targets = torch.tensor(targets, dtype=torch.float32)
+            targets = torch.tensor(targets, dtype=torch.long)
         else:
-            targets = targets.to(torch.float32)
+            targets = targets.to(torch.long)
 
         self.accuracy.update(outputs, targets)
         accuracy = self.accuracy.compute()
